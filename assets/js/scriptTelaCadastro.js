e7dac341c74f525c41e8b83bfe5f88a39daf507d
GustavoNbs <nebesgustavo@gmail.com>
2024-04-21 17:09:24 -0300

    JS

--- assets/js/scriptTelaCadastro.js
+++ assets/js/scriptTelaCadastro.js
@@ -4,16 +4,37 @@ function adicionaDadosAluno() {
     let registroaluno = document.getElementById("input_ra").value;
     let email = document.getElementById("input_email").value;
 
-    let aluno = {
-        nome: nome,
-        registroaluno: registroaluno,
-        email: email
-    };
+    switch(false) {
+        case (isNaN(nome) || !isNaN(registroaluno)):
+        alert ("Siga as regras:");
+        break;
 
-    let informacaoaluno = JSON.stringify(aluno);
+        // case (isNaN(nome)):
+        // break;
+        
+        
+        default:
+        let aluno = {
+            nome: nome,
+            registroaluno: registroaluno,
+            email: email
+        };
+    
+        let informacaoaluno = JSON.stringify(aluno);
+    
+        localStorage.setItem('aluno', informacaoaluno);
 
-    localStorage.setItem('aluno', informacaoaluno);
+        console.log("Dados do aluno salvos");
 
-    console.log("Dados do aluno salvos");
+        let dadosSalvos = localStorage.getItem('aluno');
+        let alunoSalvo =  JSON.parse(dadosSalvos);
+        console.log(alunoSalvo)
+        }
+
+    
+
+   
+
+    
 }
 
