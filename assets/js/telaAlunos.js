95bffb3cfb724b14e6cce5a89f2ceb068445bfb6
MaykeCorreia <mayke_correia@hotmail.com>
2024-04-24 21:03:19 -0300

    Atualização Tabela tela alunos Json

--- assets/js/telaAlunos.js
+++ assets/js/telaAlunos.js
@@ -1,48 +1,23 @@
-function mostrarInformacoes() {
-    let dropdown = document.getElementById("alunos");
-    let alunoSelecionado = dropdown.options[dropdown.selectedIndex].value;
 
-    var nomealuno = "";
-    var registroaluno = "";
-    var emailaluno = "";
+function carregarDadosAlunos() {
+    if (localStorage.getItem('aluno')) {
+        let dadosSalvos = localStorage.getItem('aluno');
+        let alunoSalvo = JSON.parse(dadosSalvos);
 
+        let tabelaAlunos = document.getElementById('tabela_alunos');
 
-    switch (alunoSelecionado) {
-        case "aluno1":
-            nomealuno = "Aluno 1";
-            registroaluno = "87361138";
-            emailaluno = "aluno139@yahoo.com";
-            break;
-        case "aluno2":
-            nomeAluno = "Aluno 2";
-            registroaluno = "90626832";
-            emailaluno = "aluno232@gmail.com";
-            break;
-        case "aluno3":
-            nomealuno = "Aluno 3";
-            registroaluno = "31143455";
-            emailaluno = "aluno393@outlook.com";
-            break;
-        case "aluno4":
-            nomealuno = "Aluno 4";
-            registroaluno = "25773642";
-            emailaluno = "aluno439@gmail.com";
-            break;
-        case "aluno5":
-            nomealuno = "Aluno s5";
-            registroaluno = "06397615";
-            emailaluno = "aluno526@outlook.com";
-            break;                        
-        default:
-            alert("Selecione um aluno válido!");
-            return;
+        let novaLinha = document.createElement('tr');
+        novaLinha.innerHTML = `
+            <td>${alunoSalvo.nome}</td>
+            <td>${alunoSalvo.registroaluno}</td>
+            <td>${alunoSalvo.email}</td>
+        `;
+        tabelaAlunos.appendChild(novaLinha);
+    } else {
+        console.log("Nenhum dado de aluno encontrado no localStorage.");
     }
-
-
-    document.getElementById("nomealuno").textContent = nomealuno;
-    document.getElementById("registroaluno").textContent = registroaluno;
-    document.getElementById("emailaluno").textContent = emailaluno;
-
-
-    document.getElementById("informacoesAluno").style.display = "block";
 }
+
+window.onload = function() {
+    carregarDadosAlunos();
+};
